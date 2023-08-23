0eaba5dda0c136cf33b7f77de12e4d8cce6a97a1
Jerimaine Hunt <100004627+JeriJamz@users.noreply.github.com>
2023-08-23 15:33:34 -0500

    Create BAsics.m
    This might get expanded on

---
+++ BAsics.m
@@ -0,0 +1,12 @@
+#include<foundation/foundation.h>
+
+int main(int argc, const char *argv[]){
+
+    @autoreleasepool{//this is where the main fun go I think
+
+        NSString *name = [_txtName text] //wtf is up wit all the pointers. This is string declaration btw
+        NSLog(@"Hello World")
+    
+    }
+
+}
